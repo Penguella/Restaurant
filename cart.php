af909e66942926768bab4f069dca4c5f97883d36
MihaelaMihai <72732315+Penguella@users.noreply.github.com>
2022-12-13 17:08:38 +0200

    Add files via upload
    Versiunea 1.0

---
+++ cart.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>FoodPenguin</title>
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
+
+            *{
+                font-family: 'Titillium Web', sans-serif;
+            }
+        </style>
+    </head>
+    
+    <body>
+       <!--pt navbar-->
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="registration.php">Register</a></li>
+            <li><a href="menu.php">Food index</a></li>
+            <li><a class="active" href="cart.php">Cart</a></li>
+        </ul>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,73 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>FoodPenguin</title>
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="style.css">
+
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Titillium+Web');
+
+            *{
+                font-family: 'Titillium Web', sans-serif;
+            }
+        </style>
+    </head>
+    
+    <body>
+       <!--pt navbar-->
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a class="active" href="login.php">Login</a></li>
+            <li><a href="registration.php">Register</a></li>
+            <li><a href="menu.php">Food index</a></li>
+            <li><a href="cart.php">Cart</a></li>
+        </ul>
+        <br>
+        <?php
+    require('connection.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['username'])) {
+        $username = stripslashes($_REQUEST['username']);    // removes backslashes
+        $username = mysqli_real_escape_string($conn, $username);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($conn, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM users WHERE username='$username'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($conn, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $_SESSION['username'] = $username;
+            // Redirect to user dashboard page
+            header("Location: dashboard.php");
+        } else {
+            echo "<div class='form'>
+                  <h3>Incorrect Username/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="registration.php">New Registration</a></p>
+  </form>
+<?php
+    }
+?>
+    </body>
+</html>
